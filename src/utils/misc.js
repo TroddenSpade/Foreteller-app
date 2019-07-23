9eeaf4033a185bbe9e8d4b5d090e67c1b3fc39ba
TroddenSpade <parsasam1378@gmail.com>
2019-07-24 00:30:50 +0430

    get set and remove token functions added

---
+++ src/utils/misc.js
@@ -0,0 +1,32 @@
+import {AsyncStorage} from 'react-native';
+
+export const setToken = (data)=>{
+    console.log(data)
+    AsyncStorage.multiSet([
+        ['@Foreteller@id',data._id],
+        ['@Forteller@token',data.token]
+    ]).then(()=>console.log("Token Has Been Set"));
+}
+
+export const getToken = ()=>{
+    return AsyncStorage.multiGet([
+        '@Foreteller@id',
+        '@Forteller@token'
+    ]).then(res=>{
+        if(res[0][1] && res[1][1])
+            return {
+                id: res[0][1],
+                token: res[1][1]
+            };
+        else
+            return null;
+    })
+    .catch(e=>console.log(e));
+}
+
+export const removeToken =(CBfunction)=>{
+    AsyncStorage.multiRemove([
+        '@Foreteller@id',
+        '@Forteller@token'
+    ]).then(()=>CBfunction());
+}
