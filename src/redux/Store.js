e474463ca6ebc91326764ffe732b9dfd61dcc0b2
TroddenSpade <parsasam1378@gmail.com>
2019-07-24 00:33:29 +0430

    redux promise added

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,10 +1,11 @@
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise'
 
 import Reducers from './reducers';
 
 const Store = createStore(
     Reducers,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk,promise)
 );
 export default Store;
