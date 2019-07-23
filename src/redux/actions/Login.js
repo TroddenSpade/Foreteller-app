4ba87ae4e504a1af9b6ff450d06d0f410a641139
TroddenSpade <parsasam1378@gmail.com>
2019-07-24 00:34:13 +0430

    Update Login.js

--- src/redux/actions/Login.js
+++ src/redux/actions/Login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { SIGN_UP_ADD,SIGN_IN_ADD } from '../links';
-import { SIGN_UP,SIGN_IN } from '../types';
+import { SIGN_UP_ADD,SIGN_IN_ADD,CHECK_TOKEN_ADD } from '../links';
+import { SAVE_USER } from '../types';
 
 
 export const signUp =(data,sCB)=>{
@@ -8,7 +8,7 @@ export const signUp =(data,sCB)=>{
         axios.post(SIGN_UP_ADD,data)
         .then(response =>{
             if(response.data.signup){
-                dispatch({type:SIGN_UP,payload:response.data})
+                dispatch({type:SAVE_USER,payload:response.data})
                 sCB();
             }else{
                 console.log(response.data.err);
@@ -18,13 +18,27 @@ export const signUp =(data,sCB)=>{
     }
 }
 
-export const signIn=(data)=>{
+export const signIn=(data,sCB)=>{
     return (dispatch) =>{
         axios.post(SIGN_IN_ADD,data)
         .then(response=>{
             if(response.data.isAuth){
-                console.log(response,"payload data");
-                dispatch({type:SIGN_IN,payload:response.data})
+                dispatch({type:SAVE_USER,payload:response.data})
+                sCB(response.data);
+            }else{
+                console.log(response.data.err);
+            }
+        })
+        .catch(err=>console.log(err))
+    }
+}
+
+export const checkToken =(data)=>{
+    return (dispatch) =>{
+        axios.post(CHECK_TOKEN_ADD,data)
+        .then(response=>{
+            if(response.data.valid){
+                dispatch({type:SAVE_USER,payload:response.data})
             }else{
                 console.log(response.data.err);
             }
