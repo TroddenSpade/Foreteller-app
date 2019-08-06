e8cb7178e47bcaa1466f54896292619669a6a4d3
TroddenSpade <parsasam1378@gmail.com>
2019-08-06 15:41:07 +0430

    slider added

---
+++ ios/build/Foreteller/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/react-native-slider.build/Objects-normal/x86_64/RNCSlider.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/parsasam/Documents/github/Foreteller-app/node_modules/@react-native-community/slider/ios/RNCSlider.m \
+  /Users/parsasam/Documents/github/Foreteller-app/ios/Pods/Target\ Support\ Files/react-native-slider/react-native-slider-prefix.pch \
+  /Users/parsasam/Documents/github/Foreteller-app/node_modules/@react-native-community/slider/ios/RNCSlider.h \
+  /Users/parsasam/Documents/github/Foreteller-app/ios/Pods/Headers/Public/React-Core/React/RCTComponent.h
